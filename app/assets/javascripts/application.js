dab6bc4f61c9c4f6d03badefbdc8dcab23842370
tanapon.aree@gmail.com <tanapon.aree@gmail.com>
2016-04-24 13:22:15 +0700

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require bootstrap_sb_admin_base_v2
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require bootstrap_sb_admin_base_v2
