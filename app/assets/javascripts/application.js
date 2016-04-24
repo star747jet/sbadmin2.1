f53e1033616102fdf96ab870de46a8ea4f4e39a4
tanapon.aree@gmail.com <tanapon.aree@gmail.com>
2016-04-24 16:03:32 +0700

    2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,10 @@
 //
 
 //= require bootstrap
+
+//= require bootstrap_sb_admin_base_v2
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap_sb_admin_base_v2
 //= require turbolinks
 
 
